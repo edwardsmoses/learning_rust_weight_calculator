c55ae27bd152532b0c6bd27ea64c23264f5876d7
Edwards Moses <edwardsmoses3@gmail.com>
2022-07-22 14:00:00 +0100

    Get user weight from readline and convert it using the calc function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,24 @@
+use std::io;
+
+
 fn main() {
-    let mut weight_on_mars = calculate_weight_on_mars(100.0);
-    weight_on_mars = weight_on_mars * 1000.0;
-    println!("Weight on Mars: {}kg", weight_on_mars);
+
+    let mut weight_input = String::new();
+
+    println!("What's your current Weight?");
+
+    let result = io::stdin().read_line(&mut weight_input);
+    match result {
+        Ok(n) => {
+            let mut weight_on_mars = calculate_weight_on_mars(n as f32);
+            weight_on_mars = weight_on_mars * 1000.0;
+            println!("Weight on Mars: {}kg", weight_on_mars);
+        }
+        Err(error) => {
+            println!("error: {error}")
+        },
+    };
+
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
