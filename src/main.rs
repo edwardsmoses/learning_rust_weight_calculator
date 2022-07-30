30abdc13ce995572ca2117fba4058f3a5f7e213b
Edwards Moses <edwardsmoses3@gmail.com>
2022-07-30 14:00:00 +0100

    print the calc function result

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
 fn main() {
-    println!("Hello, world!, Number: {}, String: {}", 100, "abcd is passed");
-
-    calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {}kg", calculate_weight_on_mars(100.0));
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
