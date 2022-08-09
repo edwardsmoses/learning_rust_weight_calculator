ad5b294716803ad206abe8372a7d7740af128a17
Edwards Moses <edwardsmoses3@gmail.com>
2022-08-09 18:01:21 +0100

    Understanding the ownership and borrowing system in Rust

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,29 @@
 use std::io;
 
-
 fn main() {
-
     let mut weight_input = String::new();
 
     println!("What's your current Weight?");
 
-    let result = io::stdin().read_line(&mut weight_input);
-    match result {
-        Ok(n) => {
-            let mut weight_on_mars = calculate_weight_on_mars(n as f32);
-            weight_on_mars = weight_on_mars * 1000.0;
-            println!("Weight on Mars: {}kg", weight_on_mars);
-        }
-        Err(error) => {
-            println!("error: {error}")
-        },
-    };
+    io::stdin().read_line(&mut weight_input).unwrap();
 
+    let mut weight_on_mars = calculate_weight_on_mars(weight_input.trim().parse().unwrap());
+    weight_on_mars = weight_on_mars * 1000.0;
+    println!("Weight on Mars: {}kg", weight_on_mars);
+   
+
+    borrow_string(&weight_input);
+    own_string(weight_input);
 }
 
 fn calculate_weight_on_mars(weight: f32) -> f32 {
-    (weight / 9.81)  * 3.711
+    (weight / 9.81) * 3.711
+}
+
+fn borrow_string(s: &String) {
+    println!("{}", s)
+}
+
+fn own_string(s: String) {
+    println!("{}", s)
 }
